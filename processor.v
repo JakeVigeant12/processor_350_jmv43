7d822ec52896b41cfdea97c5c1911a078cb3630a
Jake Vigeant <108590230+JakeVigeantProfessional@users.noreply.github.com>
2023-03-19 23:34:15 -0400

    weird ALU error

--- processor.v
+++ processor.v
@@ -64,7 +64,7 @@ module processor(
 
 	/* YOUR CODE STARTS HERE */
 
-    //PC 
+//PC 
     wire [31:0] pcActive, pcAdv, fd_pc_out, fd_ir_out;
     //module pc_reg(clock, reset, in_enable, in, out);
     //load next pc into 
@@ -73,9 +73,10 @@ module processor(
     //module cla_full_adder(a, b, c_in, s);\
     cla_full_adder inc_pc(pcActive, 1, 1'b0, pcAdv); 
 
-    //FD stage
+//FD stage
     //Disable enable toggle if time to stall later
-    fd_latch fd(clock, 1'b1, pcActive, q_imem, fd_out, fd_ir_out);
+    //module fd_latch(clk, enable, cPc, inIns, pcOut, insOut);
+    fd_latch fd(clock, 1'b1, pcActive, q_imem, fd_pc_out, fd_ir_out);
 
     wire [4:0] fd_opcode;
     assign fd_opcode = fd_ir_out[31:27];
@@ -92,24 +93,26 @@ module processor(
     //If not r type, just read reg0
     assign ctrl_readRegB = fd_isR ? fd_ir_out[16:12] : 5'b0;
 
-    //DX stage
+
+
+//DX stage
     wire [31:0] dx_ir_in, dx_pcOut, dx_a_curr, dx_b_curr, dx_ir_out;
-    assign dx_ir_in = fd_ir_out;
-    dx_latch dx(clock, fd_out, data_readRegA, data_readRegB, dx_ir_in, dx_pcOut, dx_a_curr, dx_b_curr, dx_ir_out);
+    //module dx_latch(clk, cPc, a_in, b_in, inIns, pcOut, aOut, bOut, insOut);
+    dx_latch dx(clock, fd_pc_out, data_readRegA, data_readRegB, fd_ir_out, dx_pcOut, dx_a_curr, dx_b_curr, dx_ir_out);
 
     // get operation for execute stage
     wire [4:0] dx_opcode;
     assign dx_opcode = dx_ir_out[31:27];
 
-    // Feed into XM stage
-    wire [1:0] mux_b;
-    wire [31:0] xm_o_curr;
+    // // Feed into XM stage
+    // wire [1:0] mux_b;
+    // wire [31:0] xm_o_curr;
 
     wire [31:0] inp_a, inp_b;
     wire [31:0] alu_b_mux_out;
+    //ALU input A is always just A in basic case
     assign inp_a = dx_a_curr;
-    //module mux_4(in0, in1, in2, in3, out, sel);
-    mux_4 alumux1(xm_o_curr, data_writeReg, dx_b_curr, 32'b0, alu_b_mux_out, mux_b);
+
 
     //Get immediate value
     wire [31:0] imm;
@@ -117,63 +120,56 @@ module processor(
     //sign extend the imm
     assign imm[31:17] = dx_ir_out[16] ? 15'b1 : 15'b0;
 
+    //Choose between the immediate and the value from regB
+    //module mux_2(out, select, in0, in1);
+    wire dx_is_I,dx_is_R;
+    assign dx_is_I = ~dx_opcode[4] & ~dx_opcode[3] & dx_opcode[2] & ~dx_opcode[1] & dx_opcode[0];
+    assign dx_is_R = ~dx_opcode[4] & ~dx_opcode[3] & ~dx_opcode[2] & ~dx_opcode[1] & ~dx_opcode[0];
 
-    // wire dx_rOp;
-    // assign dx_rOp = ~dx_opcode[4] & ~dx_opcode[3] & ~dx_opcode[2] & ~dx_opcode[1] & ~dx_opcode[0];
-   
-    // //Wire through ALU inputs, shamt, op
-    // wire [4:0] alu_opcode, shamt;
-    // assign alu_opcode = dx_ir_out[6:2];
-    // assign shamt = dx_ir_out[11:7];
-    // assign inp_b = dx_rOp ? alu_b_mux_out : imm;
+    mux_2 operandBMux(inp_b,dx_is_I,dx_b_curr,imm);
 
-    // //ALU unit and output
-    // wire [31:0] alu_out, alu_out_ovf;
-    // wire is_not_equal, is_less_than, alu_overflow;
-    // alu ula(inp_a, inp_b, alu_opcode, shamt, alu_out, is_not_equal, is_less_than, alu_overflow);
+    //Wire through ALU inputs, shamt, op
+    wire [4:0] alu_opcode, shamt;
+    assign alu_opcode = dx_ir_out[6:2];
+    assign shamt = dx_ir_out[11:7];
+
+    //ALU unit and output
+    wire [31:0] alu_out, alu_out_ovf;
+    wire is_not_equal, is_less_than, alu_overflow;
+    alu ula(inp_a, inp_b, alu_opcode, shamt, alu_out, is_not_equal, is_less_than, alu_overflow);
 
     // //MULTDIV
 
-    // //Overflow from all arithematic units
-    // wire overflow;
-    // assign overflow = alu_overflow;
+    //Overflow from all arithematic units
+    wire overflow;
+    assign overflow = alu_overflow;
 
-    // //Output from alu into xm latch
-    // wire [31:0] xm_o_in;
-    // assign xm_o_in = alu_out;
+//XM stage
+    //module xm_latch(clk, o_in, ovfIn, b_in, inIns,  o_out, outOvf, bOut, insOut);
+    wire [31:0] xm_o_out, xm_b_out, xm_ir_curr;
+    wire xm_overflow_out;
+    xm_latch xm(clock, alu_out, overflow, data_readRegB, dx_ir_out, xm_o_out, xm_overflow_out, xm_b_out, xm_ir_curr);
 
-    // // XM latch
-    // wire [31:0] xm_b_curr, xm_ir_curr;
-    // wire xm_overfl;
-    // //module xm_latch(clk, o_in, ovfIn, b_in, inIns,  o_out, outOvf, bOut, insOut);
-    // xm_latch xm(clock, xm_o_in, overflow, alu_b_mux_out, dx_ir_out, xm_o_curr, xm_overfl, xm_b_curr, xm_ir_curr);
-	
-   
+    //HANDLE data memory reads and writes here
 
-    // // MW latch
-    // wire [31:0] mw_o_out, mw_d_out, mw_ir_out;
-    // wire mw_ovf_out;
-    // //module mw_latch(clk, o_in, ovfIn, d_in, inIns,  o_out, outOvf, dOut, insOut);
-    // mw_latch mw(clock, xm_o_curr, xm_overfl, q_dmem, xm_ir_curr, mw_o_out, mw_ovf_out, mw_d_out, mw_ir_out);
+    wire[31:0] dataMemOut;
+    //0's until lw instruction is implemented
+    assign dataMemOut = 32'b0;
+    
 
-    // //writeback step
-    // wire [4:0] mw_opcode;
-    // assign mw_opcode = mw_ir_out[31:27];
-    // wire mw_is_r_type_op, mw_is_addi_op;
-    // assign mw_is_r_type_op = ~mw_opcode[4] & ~mw_opcode[3] & ~mw_opcode[2] & ~mw_opcode[1] & ~mw_opcode[0];
-    // assign mw_is_addi_op = ~mw_opcode[4] & ~mw_opcode[3] & mw_opcode[2] & ~mw_opcode[1] & mw_opcode[0];
+//MW Stage
+    wire [31:0] mw_o_out, mw_d_out, mw_ir_out;
+    wire mw_ovf_out;
+    //module mw_latch(clk, o_in, ovfIn, d_in, inIns,  o_out, outOvf, dOut, insOut);
+    mw_latch mw(clock, xm_o_out, xm_overflow_out, dataMemOut, xm_ir_curr, mw_o_out, mw_ovf_out, mw_d_out, mw_ir_out);
 
-    // assign ctrl_writeReg = mw_ir_out[26:22];
-
-
-    // assign data_writeReg =  mw_o_out;
-    // assign ctrl_writeEnable = mw_is_r_type_op | mw_is_addi_op;
-
-
-
-
+    //For now just writeback arithematic output, O
+    //With lw instruction, dmem output will be stored (use mux) 
+    assign ctrl_writeReg = mw_ir_out[26:22];
+    assign data_writeReg =  mw_o_out;
+    //Disable write enable with other instruction types as added
+    assign ctrl_writeEnable = 1'b1;
 
 
 // 	/* END CODE */
-
 endmodule

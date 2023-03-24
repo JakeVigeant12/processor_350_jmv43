242a12039b1c6d328a5ce705915714e146bd45ef
Jake Vigeant <108590230+JakeVigeantProfessional@users.noreply.github.com>
2023-03-24 13:28:42 -0400

    clean

--- rstatushandling.v
+++ rstatushandling.v
@@ -3,17 +3,17 @@ module rstatus_res(alu_op, op, rstatus_write_val);
     output [31:0] rstatus_write_val;
 
     wire is_rOp;
-    assign is_rOp = (op == 00000);
+    assign is_rOp = (op === 5'b00000);
 
     wire addi;
-    assign addi = (op == 00101);
+    assign addi = (op === 5'b00101);
 
     //If r determinne optype to select statusval
     wire add, sub, mult, div;
-    assign add = is_rOp & (alu_op == 00000);
-    assign sub = is_rOp & (alu_op == 00001);
-    assign mult = is_rOp & (alu_op == 00110);
-    assign div = is_rOp & (alu_op == 00111);
+    assign add = is_rOp & (alu_op === 5'b00000);
+    assign sub = is_rOp & (alu_op === 5'b00001);
+    assign mult = is_rOp & (alu_op === 5'b00110);
+    assign div = is_rOp & (alu_op === 5'b00111);
 
     //Select write output based on op
     //module mux_8(in0,in1,in2,in3,in4,in5,in6,in7, out, select);
